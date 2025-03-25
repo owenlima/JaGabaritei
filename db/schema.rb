0e8da38cba9d7665a0fa59a63c2d77087f41681f
owenlima <owen.liima@gmail.com>
2025-03-25 20:43:25 -0300

    configuração do perfil de usuario no devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_25_004336) do
+ActiveRecord::Schema.define(version: 2025_03_25_225109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,16 @@ ActiveRecord::Schema.define(version: 2025_03_25_004336) do
     t.integer "questions_count"
   end
 
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "address"
+    t.string "gender"
+    t.date "birthdate"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -59,10 +69,13 @@ ActiveRecord::Schema.define(version: 2025_03_25_004336) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "subjects"
+  add_foreign_key "user_profiles", "users"
 end
