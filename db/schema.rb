dc144bceaebdcb4ba0fa5bb9455f920afbae04ca
owenlima <owen.liima@gmail.com>
2025-03-27 15:05:08 -0300

    utilização da api de busca cep

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_26_131838) do
+ActiveRecord::Schema.define(version: 2025_03_27_173225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2025_03_26_131838) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zip_code"
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
